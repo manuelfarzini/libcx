b8125e868002ad0dfd2df760700fd97ee4c4548a
Manuel Farzini <manuel.farzini@gmail.com>
2026-07-02 09:27:31 +0200

    Periodic update.

---
+++ include/libcx/config.hh
@@ -0,0 +1,13 @@
+/** @file libcx/config.hh **/
+
+#ifndef CX_CONFIG_HH
+#define CX_CONFIG_HH
+
+#include "libcx/conf/hal.hh"
+#include "libcx/conf/macro.hh"
+#include "libcx/conf/prelude.hh"
+#include "libcx/conf/prelude.hh"
+#include "libcx/conf/simd.hh"
+#include "libcx/conf/type.hh"
+
+#endif  // CX_CONFIG_HH
